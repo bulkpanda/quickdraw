65d7d0195b862c0401f7b0eb168c6ba30360ae7f
Kunal PAtel <kunal.patel0999@gmail.com>
2021-03-14 00:43:30 +0530

    ''

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  SERVER_URL:"http://18.221.124.152/api"
+  SERVER_URL:"https://kunal.autonise.com/api"
   // SERVER_URL:"http://127.0.0.1:5000"
 };
